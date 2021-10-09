244bde780f983ba0922f0c7ebe037c6ae508da6d
Guillermo Ramirez <guialera@gmail.com>
2021-10-09 09:42:48 -0700

    Initial commit 1092021

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import MainComponent from "./MainComponent"
+
+function App(){
+    return(
+        <div>
+            <MainComponent />
+        </div>
+    )
+}
+
+export default App
---
+++ src/MainComponent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function MainComponent(){
+    return(
+        <div>
+            <h1 className="mainTitle">neonCode</h1>
+            <h1 className="secondaryTitle">COMING SOON</h1>
+        </div>
+    )
+}
+
+export default MainComponent
