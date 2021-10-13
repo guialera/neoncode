9dbab8c3f1fc7dd37c2c4d8ef65edbb30b8008eb
Guillermo Ramirez <guialera@gmail.com>
2021-10-13 12:57:31 -0700

    Committing an update 10/13/2021

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 
+import Welcome from "./Welcome.js"
 import About from "./About.js"
 import Deployed from "./Deployed.js"
 
 
-function MainComponent(){
-    return(
+function MainComponent() {
+
+    const [showWelcome, setShowWelcome] = useState(true)
+    const [showAbout, setShowAbout] = useState(false)
+    const [showDeployed, setShowDeployed] = useState(false)
+
+    function aboutToggle() {
+        setShowAbout(prevShowAbout => !prevShowAbout)
+        setShowWelcome(false)
+    }
+
+    function deployedToggle() {
+        setShowDeployed(prevShowDeployed => !prevShowDeployed)
+        setShowWelcome(false)
+    }
+
+    return (
         <div>
             <h1 className="mainTitle">neonCode</h1>
-            <About />
-            <Deployed />
+            <div style={{ display: showWelcome ? "block" : "none" }}>
+                <Welcome />
+            </div>
+            <div className="buttonDiv">
+                <button onClick={aboutToggle} style={{ backgroundColor: showAbout ? "#FFFF00" : "coral" }}>{"<About />"}</button>
+                <button onClick={deployedToggle} style={{ backgroundColor: showDeployed ? "#FFFF00" : "coral" }}>{"<Deployed />"}</button>
+            </div>
+            <div style={{ display: showAbout ? "block" : "none" }}>
+                <About />
+            </div>
+            <div style={{ display: showDeployed ? "block" : "none" }}>
+                <Deployed />
+            </div>
         </div>
     )
 }
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Welcome() {
+    return (
+        <div>
+            <h1 className="welcomeDiv">Welcome to neonCode...choose an option to begin your visit!</h1>
+        </div>
+    )
+}
+
+export default Welcome
